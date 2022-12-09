fb139db81424e7a00a1400e34d7031750d10ded2
MatteoBertolone <114439478+MatteoBertolone@users.noreply.github.com>
2022-12-09 21:29:53 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,9 +1,11 @@
 #include "lib.h"
+
 float max(int y, float x[]){
     float m=0;
     for(int j=0; j<y; j++){
-        if(x[j]>m)
+        if(x[j]>m){
             m=x[j];
+		}
     }
     return m;
 }
