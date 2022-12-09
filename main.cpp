95982bbf548e710388833f4c61886f01032e51df
MatteoBertolone <114439478+MatteoBertolone@users.noreply.github.com>
2022-12-09 21:24:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,8 @@
 #include "lib.h"
 int main(){
 	int l=0;
-    float a[l];
-    do{
-        cin>>l; 
-    }while(l<1);
+	float a[l];
+    cin>>l; 
     for(int i=0; i<l; i++){
         cin>>a[i];
     }
